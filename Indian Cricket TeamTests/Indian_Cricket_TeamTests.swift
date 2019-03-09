b555d12d5b49c6842300b9be50f62a9a2169046e
Maxence Henry <maxence_henry@outlook.fr>
2019-03-09 15:41:17 +0000

    Initial Commit

---
+++ Indian Cricket TeamTests/Indian_Cricket_TeamTests.swift
@@ -0,0 +1,34 @@
+//
+//  Indian_Cricket_TeamTests.swift
+//  Indian Cricket TeamTests
+//
+//  Created by Maxence Henry on 09/03/2019.
+//  Copyright © 2019 Chetan Umale. All rights reserved.
+//
+
+import XCTest
+@testable import Indian_Cricket_Team
+
+class Indian_Cricket_TeamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
